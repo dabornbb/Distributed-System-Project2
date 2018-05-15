83e3fa0a5cf75c7c9c43dfc476d9957eb9004bdd
Hannah <37783503+thuyngoch@users.noreply.github.com>
2018-05-16 00:46:37 +1000

    Add files via upload

--- src/activitystreamer/util/Commands.java
+++ src/activitystreamer/util/Commands.java
@@ -1,11 +1,16 @@
 package activitystreamer.util;
 
 import activitystreamer.server.Connection;
+import activitystreamer.server.ServerList;
 
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+
+import java.sql.Timestamp;
+
 public class Commands {
+
 
 	private static JSONObject sendObj;
 	//write message and automatically shut down connections
@@ -41,36 +46,31 @@ public class Commands {
 		con.writeMsg(sendObj.toJSONString());
 	}
 	
-	public static void loginSuccess(Connection con,String username) {
-		sendObj = new JSONObject();
-		sendObj.put("command", "LOGIN_SUCCESS");
-		sendObj.put("info","successfully login as user "+ username);
-		con.writeMsg(sendObj.toJSONString());
+	
+	public static void loginSuccess(Connection con,JSONObject obj) {
+		obj.put("command", "LOGIN_SUCCESS");
+		obj.put("info","successfully login as user "+ obj.get("username"));
+		con.writeMsg(obj.toJSONString());
 	}
 	
-	public static void loginFail(Connection con,String str) {
-		sendObj = new JSONObject();
-		sendObj.put("command", "LOGIN_FAILED");
-		sendObj.put("info", str);
-		con.writeMsg(sendObj.toJSONString());
-		con.closeCon();
+	public static void loginFail(Connection con,String str, JSONObject obj) {
+		obj.put("command", "LOGIN_FAILED");
+		obj.put("info", str);
+		con.writeMsg(obj.toJSONString());
 	}
 	
-	public static void registerSuccess(Connection con, String username,String secret) {
-		sendObj = new JSONObject();
-		sendObj.put("command", "REGISTER_SUCCESS");
-		sendObj.put("info","logged in as user "+ username+", your secret is "+secret);
-		con.writeMsg(sendObj.toJSONString());
+	public static void registerSuccess(Connection con, JSONObject obj) {
+		obj.put("command", "REGISTER_SUCCESS");
+		con.writeMsg(obj.toJSONString());
 	}
 	
-	public static void registerFail(Connection con, String username) {
-		sendObj = new JSONObject();
-		sendObj.put("command", "REGISTER_FAILED");
-		sendObj.put("info", username+" is already registered in the system");
-		con.writeMsg(sendObj.toJSONString());
+	public static void registerFail(Connection con, JSONObject obj) {
+		obj.put("command", "REGISTER_FAILED");
+		obj.put("info", obj.get("username").toString()+" is already registered in the system");
+		con.writeMsg(obj.toJSONString());
 	}
 
-	public static void serverAnnounce(Connection con,String id,int load,String hostname,int port) {
+/*	public static void serverAnnounce(Connection con,String id,int load,String hostname,int port) {
 		sendObj = new JSONObject();
 		sendObj.put("command", "SERVER_ANNOUNCE");
 		sendObj.put("id",id);
@@ -79,6 +79,7 @@ public class Commands {
 		sendObj.put("port", port);
 		con.writeMsg(sendObj.toJSONString());
 	}
+	*/
 	
 	public static void sendAuthenticate(Connection con) {
 		sendObj = new JSONObject();
@@ -87,5 +88,25 @@ public class Commands {
 		con.writeMsg(sendObj.toJSONString());
 	}
 	
+	public static void AuthenSuccess(Connection con,Timestamp time) {
+		sendObj = new JSONObject();
+		sendObj.put("command", "AUTHENTICATION_SUCCESS");
+		sendObj.put("time", time.getTime());
+		con.writeMsg(sendObj.toJSONString());
+	}
 
+	public static void redirect(Connection con, String hostname, int port,JSONObject obj) {
+		obj.put("command", "REDIRECT");
+		obj.put("hostname", hostname);
+		obj.put("port", port);
+		con.writeMsg(obj.toJSONString());
+	}
+	
+	public static void updateLoad(Connection con, String id, int load) {
+		JSONObject sendObj= new JSONObject();
+		sendObj.put("command", "UPDATE_LOAD");
+		sendObj.put("id", id);
+		sendObj.put("load", load);
+		con.writeMsg(sendObj.toJSONString());
+	}
 }
