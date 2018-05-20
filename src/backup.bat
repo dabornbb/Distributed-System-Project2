5443c9e8bdfc75f075511c5f381095a2e8da789b
Hannah <37783503+thuyngoch@users.noreply.github.com>
2018-05-20 11:41:44 +1000

    Add files via upload

---
+++ src/backup.bat
@@ -0,0 +1,1 @@
+java -classpath activitystreamer\client;activitystreamer\server;activitystreamer\util;.;..\lib\*;..\lib\apache-log4j-2.5-bin\*;..\lib\commons-cli-1.3.1\*; activitystreamer.Server -s 123 -lp 8002 -rp 8001 -rh localhost
---
+++ src/compile.bat
@@ -0,0 +1,1 @@
+javac -cp ..\lib\*;..\lib\apache-log4j-2.5-bin\*;..\lib\commons-cli-1.3.1\*; .\activitystreamer\*.java .\activitystreamer\client\*.java .\activitystreamer\server\*.java .\activitystreamer\util\*.java
